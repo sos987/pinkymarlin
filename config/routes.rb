c6375feb7fe03c97af8c0e1605aa5343efed7ee4
Alexandr Subbotin <kelionweb@gmail.com>
2012-07-23 01:08:12 +0400

    resque fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-
+require 'resque'
 
 MarlinSearcher::Application.routes.draw do
+  mount Resque::Server.new, :at => "/resque-tasks"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
