9369f8cb6abd1b3c663de433d811cb94e9ecb03a
Alexandr Subbotin <kelionweb@gmail.com>
2012-09-20 13:35:06 +0400

    Change cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ MarlinSearcher::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   config.cache_etags = true
 
